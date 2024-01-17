3a7969362e6b728879fb606cc7065877e441ea11
div-styl <gouhmada@gmail.com>
2024-01-17 11:51:07 +0100

    the project is done waiting for the checker :rocket:

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+--- Write a SQL script that creates a table users
+--- with the following columns:
+--- id, email, name
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255)
+  );
+
