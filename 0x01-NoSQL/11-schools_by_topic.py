1557c2877d5c30ded18154e2ffc2c361f3e16a32
div-styl <gouhmada@gmail.com>
2024-01-22 23:23:44 +0100

    Update!

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -2,4 +2,11 @@
 
 def schools_by_topic(mongo_collection, topic):
     """returns the list of school having a specific topic"""
-    return mongo_collection.find({"topics": topic})
+    topic = {
+            'topics': {
+                '$elemMatch': {
+                    '$eq': topic,
+                },
+            },
+        }
+    return [doc for doc in mongo_collection.find(topic)]
